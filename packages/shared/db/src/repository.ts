5f627d20cd35f690af546da1c4f53f5a226258eb
Edward Lim <ed.lim199@gmail.com>
2026-08-26 13:23:48 +1000

    updated repository contract

--- packages/shared/db/src/repository.ts
+++ packages/shared/db/src/repository.ts
@@ -2,10 +2,14 @@ import { Retailer, Category, Product } from "@grocery-tracker/domain-model";
 
 export interface ProductRepository {
   createOrUpdate(product: Product): Promise<Product>;
+  findBy<K extends Exclude<keyof Product, "currentValue" | "uid" | "category">>(key: K, value: Product[K], limit?: number): Promise<Product[]>;
+  findSimilarBy<K extends Exclude<keyof Product, "currentValue" | "uid" | "category">>(key: K, value: Product[K], limit?: number): Promise<Product[]>;
 };
 
 export interface CategoryRepository {
   createOrUpdate(category: Category): Promise<Category>;
+  findBy<K extends keyof Category>(key: K, value: Category[K], limit?: number): Promise<Category[]>;
+  findSimilarBy<K extends keyof Category>(key: K, value: Category[K], limit?: number): Promise<Category[]>;
 };
 
 export interface RetailerRepository {
