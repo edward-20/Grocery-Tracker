e1b8c393517b7defbf45f817dc9b42d1725de4d5
Edward Lim <ed.lim199@gmail.com>
2026-08-26 10:32:17 +1000

    init of grocery-scraper uses db

---
+++ apps/grocery-scraper/src/db/init.ts
@@ -0,0 +1,26 @@
+import { makeConnectionPool } from "@grocery-tracker/db";
+import { QueryResult } from "pg";
+import { readFile } from "node:fs/promises";
+import { loadConfig } from "../config/loadConfig.js";
+
+const config = loadConfig(process.env.SCRAPER_CONFIG);
+const pool = makeConnectionPool(config.database);
+
+const sql = await readFile(
+  './src/db/schema.sql',
+  'utf-8'
+)
+
+const client = await pool.connect();
+let res: QueryResult;
+try {
+  res = await client.query(sql);
+} catch(error) {
+  console.error(error);
+} finally {
+  client.release();
+}
+
+await pool.end();
+
+console.log(`Initialized Postgres database ${config.database.database}`);
