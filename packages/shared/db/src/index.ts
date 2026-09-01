02f682097a69a384758eee861235ed486bb0754b
Edward Lim <ed.lim199@gmail.com>
2026-09-01 12:24:48 +1000

    added a isInitialised function for the database

--- packages/shared/db/src/index.ts
+++ packages/shared/db/src/index.ts
@@ -1,5 +1,5 @@
 export type { ProductRepository, CategoryRepository, RetailerRepository } from "./repository.js";
 export { PostgresProductRepository, PostgresCategoryRepository, PostgresRetailerRepository } from "./postgresRepository.js";
 export { makeConnectionPool, DatabaseConfig } from "./createPool.js";
-export { initDbSchema } from "./initDbSchema.js";
+export { initDbSchema, isInitialised } from "./initDbSchema.js";
 
--- packages/shared/db/src/initDbSchema.ts
+++ packages/shared/db/src/initDbSchema.ts
@@ -16,3 +16,67 @@ export async function initDbSchema(databaseConfig: DatabaseConfig) {
   await pool.end();
   console.log(`Initialized Postgres database ${databaseConfig.database}`);
 }
+
+export type DatabaseSchemaStatus = {
+  status: "ok" | "not-initialised" | "broken"
+  reason?: any
+}
+
+export async function isInitialised(databaseConfig: DatabaseConfig): Promise<DatabaseSchemaStatus> {
+  // can be more sophisticated (checking schema of the tables aligns with what's
+  // expected, value_at_times is indeed a hypertable, timescaledb is in use)
+  const pool = makeConnectionPool(databaseConfig);
+
+  const client = await pool.connect();
+  try {
+    // unit of measurement, retailers, categories, products, product_categories, value_at_times
+    const tableExistenceResult = await pool.query(`
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public'
+    `);
+
+    const existingTables = new Set(
+      tableExistenceResult.rows.map(row => row.table_name)
+    );
+
+    const typeExistenceResult = await pool.query(`SELECT EXISTS (
+      SELECT 1
+        FROM pg_type
+        WHERE typname = 'unit_of_measurement'
+    ) AS type_exists;
+    `);
+
+    let typeExists = typeExistenceResult.rowCount === 1 ? (typeExistenceResult.rows[0].type_exists) : false;
+
+    const diagnostic = { 
+      tableExistences: {
+        retailers: existingTables.has('retailers'),
+        categories: existingTables.has('categories'),
+        products: existingTables.has('products'),
+        product_categories: existingTables.has('product_categories'),
+        value_at_times: existingTables.has('value_at_times'),
+      },
+      typeExistences: {
+        unit_of_measurement: typeExists
+      }
+    }
+
+    return {
+      status: (
+        Object.values(diagnostic.tableExistences).every(value => value === true) 
+        && diagnostic.typeExistences.unit_of_measurement
+      ) === true ? "ok" : (
+        Object.values(diagnostic.tableExistences).every(value => value === false) 
+        && !diagnostic.typeExistences.unit_of_measurement
+      ) ? "not-initialised" : "broken",
+      reason: diagnostic
+    };
+  } catch(error) {
+    throw error;
+  } finally {
+    client.release();
+    await pool.end();
+  }
+
+}
