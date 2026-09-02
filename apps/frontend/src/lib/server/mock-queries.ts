47a3f927b128c33acf1db9e418e2f4ac88924ae2
Edward Lim <ed.lim199@gmail.com>
2026-09-03 09:46:49 +1000

    in the process of mocking for dev

--- apps/frontend/src/lib/server/mock-queries.ts
+++ apps/frontend/src/lib/server/mock-queries.ts
@@ -1,28 +1,104 @@
 import type { PricePoint } from '$lib/types/price-point';
-import data from "./mock-data.json";
+import { Product, ValueAtTime, type Category } from '@grocery-tracker/domain-model';
 
-interface Product {
-	"cents": number,
-	"department": string,
-	"grams": number,
-	"id": string,
-	"name": string,
-	"store": string,
-	"time": string
+
+const CATEGORIES: Category[] = [
+	{
+	  retailer: "Woolworths",
+	  retailerDesignatedCategoryId: "vn-9e",
+	  name: "Fruits and Vegs",
+	  path: "/browse/fruit-veg"
+	},
+	{
+	  retailer: "Woolworths",
+	  retailerDesignatedCategoryId: "v0wd03",
+	  name: "Meat and Bread",
+	  path: "/browse/meat-bread"
+	},
+	{
+	  retailer: "Woolworths",
+	  retailerDesignatedCategoryId: "sne9g239",
+	  name: "Cheese",
+	  path: "/browse/cheese"
+	},
+	{
+	  retailer: "Woolworths",
+	  retailerDesignatedCategoryId: "sdj0v-g03-",
+	  name: "Pots",
+	  path: "/browse/pots"
+	},
+	{
+	  retailer: "Coles",
+	  retailerDesignatedCategoryId: "acn",
+	  name: "Major",
+	  path: "/major"
+	},
+	{
+	  retailer: "Coles",
+	  retailerDesignatedCategoryId: "apdnv3940fj",
+	  name: "Immanuel",
+	  path: "/immanuel"
+	},
+	{
+	  retailer: "Coles",
+	  retailerDesignatedCategoryId: "bdnv-9",
+	  name: "Hotel",
+	  path: "/hotel"
+	},
+	{
+	  retailer: "Coles",
+	  retailerDesignatedCategoryId: "fewnv9",
+	  name: "Seafood",
+	  path: "/seafood"
+	},
+	{
+	  retailer: "Coles",
+	  retailerDesignatedCategoryId: "sd",
+	  name: "Failure",
+	  path: "/failure"
+	},
+];
+
+function getRandomElemFromArray<T>(arr: Array<T>): T {
+	const l = arr.length;
+	const randInd = Math.floor(Math.random() * l);
+	return arr[randInd];
 }
 
-const MOCK_PRODUCTS: Product[]= data as Product[];
+function generateRandomString(length: number) {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
-const DEPARTMENTS = [
-	'Fruit & Vegetables',
-	'Dairy & Eggs',
-	'Meat & Seafood',
-	'Bakery',
-	'Pantry',
-	'Frozen',
-	'Beverages',
-	'Snacks'
-];
+function generateRandomValueAtTime(): ValueAtTime {
+  return new ValueAtTime(generateRandomString(3), Number((Math.random()*100).toFixed(2)), new Date());
+}
+
+const MOCK_PRODUCTS: Product[]= Array.from({length: 500}).map(() => {
+	return new Product({
+		retailer: Math.random() > 0.5 ? "Woolworths" : "Coles",
+	  	retailerProductId: generateRandomString(10),
+	  	category: getRandomElemFromArray(CATEGORIES),
+
+	  	currentValue: generateRandomValueAtTime(),
+	  	name: generateRandomString(7),
+	  	path: generateRandomString(7),
+	  	description: generateRandomString(15),
+	  	brand: Math.random() > 0.5 ? generateRandomString(7) : undefined,
+	  	imageUrl: Math.random() > 0.5 ? generateRandomString(7) : undefined,
+		crossProductIdentity: Math.random() > 0.5 ? {
+		  crossRetailerId: generateRandomString(5),
+		  gtinFormat: 13
+		} : undefined
+	})
+});
+
 
 export function mockNameSearch(query: string): string[] {
 	const lower = query.toLowerCase();
