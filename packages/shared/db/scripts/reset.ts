08b144b1d11f637a6fd25fac9f32fd014ac6dddf
Edward Lim <ed.lim199@gmail.com>
2026-09-04 09:36:05 +1000

    reset script made

--- packages/shared/db/scripts/reset.ts
+++ packages/shared/db/scripts/reset.ts
@@ -1,20 +1,35 @@
-// remove all schemas
 import { Pool } from "pg";
-
+import { readFile } from "fs/promises";
+import { readdir } from "fs/promises";
 
 try {
-  if (process.env.PORT === undefined || process.env.DB === undefined || process.env.USER === undefined || process.env.PASSWORD === undefined) {
+  if (process.env.HOST === undefined || process.env.PORT === undefined || process.env.DATABASE === undefined || process.env.USER === undefined || process.env.PASSWORD === undefined) {
     throw ".env file wasn't written"
   }
   const pool = new Pool({
-    host: "localhost",
+    host: process.env.HOST,
     port: Number(process.env.PORT),
-    database: process.env.DB,
+    database: process.env.DATABASE,
     user: process.env.USER,
     password: process.env.PASSWORD
   });
 
-  const client = pool.connect();
+
+  const client = await pool.connect();
+
+  // remove all current schemas and data
+  await client.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+
+  // run the schema
+  const baseSchema = await readFile(new URL("../src/schema.sql", import.meta.url), "utf8");
+  await client.query(baseSchema);
+
+  // run the migrations
+  for (const file of (await readdir(new URL("../migrations/schema.sql", import.meta.url))).sort()) {
+    const migration = await readFile(new URL(`../migrations/${file}`, import.meta.url), "utf8");
+    await client.query(migration);
+  }
+
 } catch (error) {
   console.error(error);
 }
