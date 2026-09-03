62aa2ced9879c753f852ae732002e5bb391452cc
Edward Lim <ed.lim199@gmail.com>
2026-09-03 19:57:39 +1000

    vite and sveltekit of frontend package read environment variables from root

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,4 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { loadEnv } from 'vite';
+
+const env = loadEnv('development', '../../', '');
+
+console.log(env);
+
 export default defineConfig({ plugins: [tailwindcss(), sveltekit()], envDir: '../../' });
