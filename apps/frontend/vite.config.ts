74ac7b787e92e3d10b446c564ab19d898ba6d1b7
Edward Lim <ed.lim199@gmail.com>
2026-09-17 19:09:36 +1000

    frontend configured to use .env of the package directory

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,8 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-import { loadEnv } from 'vite';
-
-const env = loadEnv('development', '../../', '');
-
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()], envDir: '../../' });
+export default defineConfig({ 
+	plugins: [tailwindcss(), sveltekit()],
+	envDir: import.meta.dirname
+});
